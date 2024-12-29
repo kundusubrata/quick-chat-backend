2f163841cf75083768ae894da652d7d9322736ab
kundusubrata <kundu.subrata2020@outlook.com>
2024-12-29 13:18:01 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import express, { Application, Request, Response } from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app: Application = express();
+const PORT = process.env.PORT || 7000;
+import Routes from "./routes/index.js";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { setupSocket } from "./socket.js";
+import { createAdapter } from "@socket.io/redis-streams-adapter";
+import redis from "./config/redis.config.js";
+import { instrument } from "@socket.io/admin-ui";
+
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [process.env.CLIENT_APP_URL, "https://admin.socket.io"],
+    credentials: true,
+  },
+  adapter: createAdapter(redis),
+});
+
+instrument(io, {
+  auth: false,
+  mode: "development",
+});
+
+export { io };
+setupSocket(io);
+
+// * Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("It's working 🙌");
+});
+
+// * Routes
+app.use("/api", Routes);
+
+
+
+server.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
---
+++ src/socket.ts
@@ -0,0 +1,35 @@
+import { Server, Socket } from "socket.io";
+import prisma from "./config/db.config.js";
+// import { produceMessage } from "./helper.js";
+
+interface CustomSocket extends Socket {
+  room?: string;
+}
+export function setupSocket(io: Server) {
+  io.use((socket: CustomSocket, next) => {
+    const room = socket.handshake.auth.room || socket.handshake.headers.room;
+    if (!room) {
+      return next(new Error("Invalid room"));
+    }
+    socket.room = room;
+    next();
+  });
+
+  io.on("connection", (socket: CustomSocket) => {
+    // * Join the room
+    socket.join(socket.room);
+
+    // console.log("A user connected..", socket.id);
+    socket.on("message", async (data) => {
+      console.log("Server side message", data);
+      await prisma.chats.create({
+        data: data,
+      });
+      socket.to(socket.room).emit("message", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnected:", socket.id);
+    });
+  });
+}
